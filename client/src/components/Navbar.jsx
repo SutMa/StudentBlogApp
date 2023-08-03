1ae2ff45145e02f0351a9e054f62f5f482025548
SutMa <odineyez@gmail.com>
2023-08-03 15:15:17 -0600

    Working on the Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Logo from "../img/BlogLogo.png"
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="container">
+        <div className="logo"><img src={Logo} alt="" /></div>
+        <div className="links">
+          <Link className='link' to='/?cat=cs'>
+            <h6>CS</h6>
+          </Link>
+          <Link className='link' to='/?cat=design'>
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className='link' to='/?cat=engr'>
+            <h6>ENGR</h6>
+          </Link>
+          <Link className='link' to='/?cat=cs'>
+            <h6>CS</h6>
+          </Link>
+          <Link className='link' to='/?cat=cs'>
+            <h6>CS</h6>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
