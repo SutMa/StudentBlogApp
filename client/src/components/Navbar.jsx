051adb2dab7a9df111bc5f8214a142c456bf3608
SutMa <odineyez@gmail.com>
2023-08-03 15:24:28 -0600

    Need to fix navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,12 +17,17 @@ function Navbar() {
           <Link className='link' to='/?cat=engr'>
             <h6>ENGR</h6>
           </Link>
-          <Link className='link' to='/?cat=cs'>
-            <h6>CS</h6>
+          <Link className='link' to='/?cat=math'>
+            <h6>MATH</h6>
           </Link>
-          <Link className='link' to='/?cat=cs'>
-            <h6>CS</h6>
+          <Link className='link' to='/?cat=jobs'>
+            <h6>JOBS</h6>
           </Link>
+          <span>JOhn</span>
+          <span>Logout</span>
+          <span className='write'>
+            <Link to='/write'>Write</Link>
+          </span>
         </div>
       </div>
     </div>
