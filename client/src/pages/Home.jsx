93d2534629047cba1937fcc40267b6b6d7a59588
SutMa <odineyez@gmail.com>
2023-08-04 19:44:54 -0600

    Need to finish up home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,30 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import img from '../img/writing.jpg'
 
 const posts = [
   {
     id:1,
     title:'Post One',
     desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad autem eaque iure soluta, fugiat ducimus, ratione facere mollitia veniam quas explicabo, esse exercitationem beatae dignissimos earum quo id. Incidunt, autem.',
-    img: 'fakeimage.png'
+    img: img
   },
   {
     id:1,
     title:'Post One',
     desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad autem eaque iure soluta, fugiat ducimus, ratione facere mollitia veniam quas explicabo, esse exercitationem beatae dignissimos earum quo id. Incidunt, autem.',
-    img: 'fakeimage.png'
+    img: img
   },
   {
     id:1,
     title:'Post One',
     desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad autem eaque iure soluta, fugiat ducimus, ratione facere mollitia veniam quas explicabo, esse exercitationem beatae dignissimos earum quo id. Incidunt, autem.',
-    img: 'fakeimage.png'
+    img: img
   },
   {
     id:1,
     title:'Post One',
     desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad autem eaque iure soluta, fugiat ducimus, ratione facere mollitia veniam quas explicabo, esse exercitationem beatae dignissimos earum quo id. Incidunt, autem.',
-    img: 'fakeimage.png'
+    img: img
   }
 
 ]
@@ -37,14 +38,15 @@ const Home = () => {
         {posts.map(post=>(
           <div className="post" key={post.id} >
             <div className="img">
-              <img src="" alt="" />
+              <img src={post.img} alt="" />
             </div>
             <div className="content">
-              <Link to={`post/${post.id}`}>
+              <Link className='link' to={`post/${post.id}`}>
                 <h1>{post.title}</h1>
+              </Link>
                 <p>{post.desc}</p>
                 <button>Read More</button>
-              </Link>
+              
             </div>
           </div>
         )
