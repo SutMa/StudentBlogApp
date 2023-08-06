2aaa956b2750689ddbd2cdf8acc6b6d5cc81f676
SutMa <odineyez@gmail.com>
2023-08-06 13:28:20 -0600

    Setup Routes and Controllers Folder

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
-const placeHolder = 1;
+import express from 'express'
+import authRoutes from "./routes/posts.js"
+import userRoutes from "./routes/posts.js"
+import postRoutes from "./routes/posts.js"
+
+const app = express()
+app.use(express.json())
+app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/posts', postRoutes)
+
+
+app.listen(8800,()=>{
+  console.log('Connected')
+})
