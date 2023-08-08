857240a25662f4102d528980694239999c80061d
SutMa <odineyez@gmail.com>
2023-08-07 19:07:51 -0600

    Setup Reigster Page Logic

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -5,15 +5,23 @@ const Register = () => {
 
   const [input,setInput] = useState({
     username: '',
+    email: '',
+    password: '',
   })
- 
+
+  const handleChange = e =>{
+    setInputs(prev=>({...prev, [e.traget.name]: e.target.value}))
+  }
+  console.log(inputs)
+
+
   return (
     <div className='auth'>
       <h1>Register</h1>
       <form >
-        <input required type="text" placeholder='username'/>
-        <input required type="email" placeholder='email'/>
-        <input required type="password" placeholder='password'/>
+        <input required type="text" placeholder='username' name='username' onChange={handleChange}/>
+        <input required type="email" placeholder='email' name='email' onChange={handleChange}/>
+        <input required type="password" placeholder='password' name='password' onChange={handleChange}/>
         <button>Register</button>
         <p>This is an error!</p>
         <span>Do you have an account?<Link to='/login'>Login</Link></span>
