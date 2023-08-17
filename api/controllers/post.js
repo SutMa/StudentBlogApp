aefefd4c0ef51f9d80abdaf21c802007053e83c5
SutMa <odineyez@gmail.com>
2023-08-16 18:39:52 -0600

    Started post queries

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,19 @@
+export const getPosts = (req,res)=>{
+  const q = req.query.cat ? "SELECT * FROM posts WHERE cat=?" : "SELECT * FROM posts"
+}
+
+export const getPost = (req,res)=>{
+  res.json('from controller')
+}
+
 export const addPost = (req,res)=>{
+  res.json('from controller')
+}
+
+export const deletePost = (req,res)=>{
+  res.json('from controller')
+}
+
+export const updatePost = (req,res)=>{
   res.json('from controller')
 }
